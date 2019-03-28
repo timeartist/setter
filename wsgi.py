7fe2d2c64ab0116d1c61e39bf941a8ee97ba28da
Adi Foulger <me@adi.uno>
2019-03-28 10:52:44 -0600

    update connection string agian

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from redis import Redis
 application = Flask(__name__)
-R = Redis(host="demo-db.redis1.svc.cluster.local", port=15212)
+R = Redis(host="demo-db.redis1.svc.cluster.local", port=12345)
 #R = Redis()
 
 @application.route("/")
