dbabe028e1e5cd2f7ce2501f7e865b47d4f8a707
Adi Foulger <me@adi.uno>
2019-03-28 08:49:20 -0600

    Add redis

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from redis import Redis
 application = Flask(__name__)
+R = Redis(host="web1.redis1.svc.cluster.local", port=15638)
 
 @application.route("/")
 def hello():
-    return "Hello Redis!"
+    return R.ping()
 
 if __name__ == "__main__":
     application.run()
